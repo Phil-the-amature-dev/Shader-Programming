024663d398742ef86bed2bf4828aea323bcfa781
Phil-the-amature-dev <98315948+Phil-the-amature-dev@users.noreply.github.com>
2026-06-04 11:44:33 +0200

    initial

---
+++ Assets/BuildPillarMesh.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+
+public class BuildPillarMesh : MonoBehaviour
+{
+    MeshBuilder builder;
+    [SerializeField] Transform rootPosition;
+    [SerializeField] private float pillarHeight = 1;
+    [SerializeField] private float sectionHeight = 1;
+    [SerializeField] private float rowCount = 1;
+    [SerializeField] private float InversePoint = 1;
+    [SerializeField] private Vector3 rowOffstep = Vector3.right; 
+
+    
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        builder = new MeshBuilder();
+        CreateCube1(rootPosition.position);
+        GetComponent<MeshFilter>().mesh = builder.CreateMesh(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreatePillar()
+    {
+        float currentFloorHeight = rootPosition.position.y;
+
+    }
+
+    void CreateCube1(Vector3 rootPosition)
+    {
+        builder.Clear();
+        int length = 0;
+        for (int i = 0; i <= rowCount; i++)
+        {
+            //-----------------------VERTECIES--------------------
+            //bottom Vertecies
+           
+            int v1 = builder.AddVertex(rootPosition + new Vector3(1, 0, 1), new Vector2(0, 0));
+            int v1_2 = builder.AddVertex(rootPosition + new Vector3(1, 0, 1), new Vector2(0, 0));
+            int v1_3 = builder.AddVertex(rootPosition + new Vector3(1, 0, 1), new Vector2(1, 0)); // _2 = duplicate
+
+            int v2 = builder.AddVertex(rootPosition + new Vector3(1, 0, -1), new Vector2(1, 0));
+            int v2_2 = builder.AddVertex(rootPosition + new Vector3(1, 0, -1), new Vector2(1, 0));
+            int v2_3 = builder.AddVertex(rootPosition + new Vector3(1, 0, -1), new Vector2(0, 0));
+
+
+            int v3 = builder.AddVertex(rootPosition + new Vector3(-1, 0, 1), new Vector2(0, 1));
+            int v3_2 = builder.AddVertex(rootPosition + new Vector3(-1, 0, 1), new Vector2(1, 0));
+            int v3_3 = builder.AddVertex(rootPosition + new Vector3(-1, 0, 1), new Vector2(0, 0));
+            int v3_4 = builder.AddVertex(rootPosition + new Vector3(-1, 0, 1), new Vector2(0, 0));
+
+            int v4 = builder.AddVertex(rootPosition + new Vector3(-1, 0, -1), new Vector2(1, 1));
+            int v4_2 = builder.AddVertex(rootPosition + new Vector3(-1, 0, -1), new Vector2(1, 1));
+            int v4_3 = builder.AddVertex(rootPosition + new Vector3(-1, 0, -1), new Vector2(0, 0));
+            int v4_4 = builder.AddVertex(rootPosition + new Vector3(-1, 0, -1), new Vector2(1, 0));
+
+            //front vertecies
+            int v5 = builder.AddVertex(rootPosition + new Vector3(-1, sectionHeight, 1), new Vector2(1, 1));
+            int v5_2 = builder.AddVertex(rootPosition + new Vector3(-1, sectionHeight, 1), new Vector2(1, 1));
+            int v5_3 = builder.AddVertex(rootPosition + new Vector3(-1, sectionHeight, 1), new Vector2(0, 1));
+            int v6 = builder.AddVertex(rootPosition + new Vector3(1, sectionHeight, 1), new Vector2(0, 1));
+
+            //back vertecies
+            int v7 = builder.AddVertex(rootPosition + new Vector3(-1, sectionHeight, -1), new Vector2(0, 1));
+            int v7_2 = builder.AddVertex(rootPosition + new Vector3(-1, sectionHeight, -1), new Vector2(0, 1));
+            int v8 = builder.AddVertex(rootPosition + new Vector3(1, sectionHeight, -1), new Vector2(1, 1));
+            int v8_2 = builder.AddVertex(rootPosition + new Vector3(1, sectionHeight, -1), new Vector2(1, 0));
+
+            //right vertecies
+            int v9 = builder.AddVertex(rootPosition + new Vector3(1, sectionHeight, 1), new Vector2(1, 1));
+            int v9_2 = builder.AddVertex(rootPosition + new Vector3(1, sectionHeight, 1), new Vector2(1, 1));
+            int v9_3 = builder.AddVertex(rootPosition + new Vector3(1, sectionHeight, 1), new Vector2(1, 1));
+            int v10 = builder.AddVertex(rootPosition + new Vector3(1, sectionHeight, -1), new Vector2(0, 1));
+
+            //left vertecies
+            int v11 = builder.AddVertex(rootPosition + new Vector3(-1, sectionHeight, -1), new Vector2(1, 1));
+            int v11_2 = builder.AddVertex(rootPosition + new Vector3(-1, sectionHeight, -1), new Vector2(1, 1));
+            int v11_3 = builder.AddVertex(rootPosition + new Vector3(-1, sectionHeight, -1), new Vector2(0, 0));
+            int v12 = builder.AddVertex(rootPosition + new Vector3(-1, sectionHeight, 1), new Vector2(0, 1));
+
+            //--------------------FACES-------------------------
+            //bottom face
+            builder.AddTriangle(v4, v2, v1);
+            builder.AddTriangle(v1_2, v3, v4_2);
+
+            //front face
+            builder.AddTriangle(v1_2, v5_2, v3_2);
+            builder.AddTriangle(v6, v5, v1);
+
+            //back face
+            builder.AddTriangle(v4_3, v7, v2);
+            builder.AddTriangle(v2_2, v7_2, v8);
+
+            //right face
+            builder.AddTriangle(v2_3, v9, v1_3);
+            builder.AddTriangle(v10, v9_2, v2_3);
+
+            //left face
+            builder.AddTriangle(v3_3, v11, v4_4);
+            builder.AddTriangle(v12, v11_2, v3_4);
+
+            //top face
+            builder.AddTriangle(v9_3, v8_2, v11_3);
+            builder.AddTriangle(v11_3, v5_3, v9_3);
+            
+            rootPosition += rowOffstep;
+            length++;
+        }
+        
+    }
+}
+  
---
+++ Assets/Shader1.shader
@@ -0,0 +1,85 @@
+Shader "Unlit/Shader1"
+{
+    Properties
+    {
+        _HeightMap ("Height Map", 2D) = "white" {}
+        _WaveLength ("Wave Length", Float) = 0.1 
+        _ColorA ("Color A", Color) = (1,0,0,1)
+        _ColorB ("Color B", Color)  = (0,0,1,1)
+        _TextureBreathingSpeed ("Texture Breathing Speed", Float) = 1
+        _TextureWaveSpeed ("Texture Wave Speed", Float) = 1
+        _VectorDisplacementModifier ("Vector Displacement Modifier", Float) = 1
+        _Transparency ("Transparency", Float) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Transparent" "Queue" = "Transparent" }
+        LOD 100
+
+        Blend SrcAlpha OneMinusSrcAlpha
+        ZWrite Off
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _HeightMap;
+            float4 _HeightMap_ST;
+            float _VectorDisplacementModifier;
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex); 
+                float4 brightness;
+                float2 uvDisplacement = v.uv + float2(v.uv.x + _Time.y * 0.25,0);
+                
+                brightness = tex2Dlod(_HeightMap, float4(uvDisplacement,0,1));
+                o.vertex.y += brightness.x * _VectorDisplacementModifier;
+                o.uv = TRANSFORM_TEX(v.uv, _HeightMap);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            float _WaveLength;
+            float4 _ColorA;
+            float4 _ColorB;
+            float _TextureBreathingSpeed;
+            float _TextureWaveSpeed;
+            float _Transparency;
+            fixed4 frag (v2f i) : SV_Target
+            
+            {
+                
+                
+                //WAVE PATTERN
+                fixed4 returnColor = float4(0,0,0,1);
+                float slant = float(fmod(_TextureWaveSpeed * ((i.uv.x + i.uv.y) + (sin(_Time.y) * _TextureWaveSpeed)),_WaveLength)); // the higher, the more to the right
+
+                returnColor += float4(lerp(_ColorA,_ColorB,slant * sin(_Time.y) * _TextureBreathingSpeed));
+                returnColor.w = _Transparency;
+                                
+                
+                return returnColor;
+            }
+            ENDCG
+        }
+    }
+}
